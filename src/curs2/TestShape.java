dda73514a0c777415cbd97a8a252e653460c42fd
Stefan Iulian TUDOR <sttudor@bitdefender.com>
2021-11-22 11:03:20 +0200

    homework nr2

---
+++ src/curs2/TestShape.java
@@ -0,0 +1,11 @@
+package curs2;
+
+public class TestShape {
+
+	public static void main(String [] args) {
+		
+		Shape square = new Shape(8);
+		Shape rectangle = new Shape(4, 2);
+		Shape circle = new Shape(3.0);
+	}
+}
